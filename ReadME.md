16f1a296f7f330ae823308b875fad8f9b6e03f13
이다노 <eksh7080@gmail.com>
2022-10-17 23:56:44 +0900

    Update

--- ReadME.md
+++ ReadME.md
@@ -1,2 +1,5 @@
-에버랜드 URL 입니다. 
-https://eksh7080.github.io/Resume_study/
+에버랜드 URL 입니다. <br/>
+<h1>https://eksh7080.github.io/Resume_study/</h1><br/>
+
+실제 에버랜드 사이트와 비교해서 보시면 더 재밌어요!!
+<h2>https://www.everland.com/web/everland/main.html</h2>
--- js/index.js
+++ js/index.js
@@ -3,7 +3,64 @@
 // 가운데 이미지
 const article = document.querySelector(".mainLogo");
 const img = document.querySelector(".mainLogo>img");
-const h1 = document.querySelector(".mainLogo h1");
+const h2 = document.querySelector(".mainLogo>h2");
+
+console.log(article, img, h2)
+
+let chnageImg = true;
+
+window.setInterval(function(){
+  chnageImg ? (
+    h2.classList.toggle("fadeOn"),
+    img.classList.toggle("fadeExit"),
+    chnageImg = !chnageImg
+  ) : (
+    img.classList.toggle("fadeExit"),
+    h2.classList.toggle("fadeOn"),
+    chnageImg = !chnageImg
+    )
+  
+}, 10000)
+
+
+  
+    // if(chnageImg){
+    //   h2.classList.add('.fadeOn');
+    //   img.classList.remove('.fadeOn');
+    //   img.classList.add('.fadeExit');
+    //   chnageImg = !chnageImg
+    // }
+    
+    // if(!chnageImg){
+    //   h2.classList.remove('.fadeOn');
+    //   h2.classList.add('.fadeExit');
+    //   img.classList.add('.fadeOn');
+    //   chnageImg = !chnageImg
+    // }
+
+
+function chan(){
+  // h2.style.opacity = "0" ? h2.classList.toggle(".fadeOn") : img.classList.toggle(".fadeExit") 
+  // if(h2.style.opacity = "0"){
+  //   h2.classList.add(".fadeOn");
+  //   img.classList.add(".fadeExit");
+  // }
+
+      h2.classList.add('.fadeOn');
+      img.classList.remove('.fadeOn');
+      img.classList.add('.fadeExit');
+      chnageImg = !chnageImg
+}
+
+function time(){
+      h2.classList.remove('.fadeOn');
+      h2.classList.add('.fadeExit');
+      img.classList.add('.fadeOn');
+      chnageImg = !chnageImg
+}
+
+
+
 
 // gnb 슬라이드
 const gnb = document.querySelector(".gnb ul");
@@ -13,11 +70,11 @@ const moveBtn = document.querySelectorAll(".moveWrap > ul li button");
 
 const arr = Array.from(moveBtn);
 
-function move() {}
+// function move() {}
 
-for (let i = 0; i < arr.length; i++) {
-  arr[i].addEventListener("click", move);
-}
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i].addEventListener("click", move);
+// }
 
 let subToggle = true;
 
